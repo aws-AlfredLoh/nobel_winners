0c1e53d4a2ad50d6a2d41ea8878d7501c4ea2ce7
Alfred <aws.jiayong0226@gmail.com>
2021-04-03 21:32:44 +0800

    nobel_winners/nobel_winners.json added and nobel_winners/nobel_winners/spiders/nwinners_list_spider.py parse function modified.

--- nobel_winners/nobel_winners/spiders/nwinners_list_spider.py
+++ nobel_winners/nobel_winners/spiders/nwinners_list_spider.py
@@ -5,21 +5,22 @@ class NWinnerItem(scrapy.Item):
     country = scrapy.Field()
     name = scrapy.Field()
     link_text = scrapy.Field()
-
+                                                  
 class NWinnerSpider(scrapy.Spider):
     name = 'nwinners_list'
     allowed_domains = ['en.wikipedia.org']
     start_urls = ["https://en.wikipedia.org/wiki/List_of_Nobel_laureates_by_country"]
 
-    def paese(self, response):
+    def parse(self, response):
 
         h3s = response.xpath('//h3')
 
-        for h2 in h3s:
+        for h3 in h3s:
             country = h3.xpath('span[@class="mw-headline"]/text()').extract()
 
+
             if country:
-                winners = h3.xpath('following--sibling::ol[1]')
+                winners = h3.xpath('following-sibling::ol[1]')
 
                 for w in winners.xpath('li'):
                     text = w.xpath('descendant-or-self::text()').extract()
