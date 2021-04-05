f1411a1a4a8e811920dfc19933350f187122d09c
Alfred <aws.jiayong0226@gmail.com>
2021-04-05 16:41:47 +0800

    ITEM_PIPELINES set up.

--- nobel_winners/nobel_winners/settings.py
+++ nobel_winners/nobel_winners/settings.py
@@ -65,7 +65,7 @@ ROBOTSTXT_OBEY = False
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'nobel_winners.pipelines.NobelWinnersPipeline': 300,
+    'nobel_winners.pipelines.DropNonPersons': 1,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
