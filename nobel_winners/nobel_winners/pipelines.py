f3d8e7082112e728e12d8b5929c78e26049fcb27
Alfred <aws.jiayong0226@gmail.com>
2021-04-05 23:14:53 +0800

    pipelines.py updated.

--- nobel_winners/nobel_winners/pipelines.py
+++ nobel_winners/nobel_winners/pipelines.py
@@ -5,7 +5,30 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import scrapy
+from scrapy.contrib.pipeline.images import ImagesPipeline
+from scrapy.exceptions import DropItem
 
-class NobelWinnersPipeline(object):
-    def process_item(self, item, spider):
+class NobelImagesPipeline(ImagesPipeline):
+
+    # def process_item(self, item, spider):
+    #     if spider.name not in ['nwinners_minibio']:
+    #         return item
+
+    def get_media_requests(self, item, info):
+
+        for image_url in item['image_urls']:
+            yield scrapy.Request(image_url)
+
+    def item_completed(self, results, item, info):
+
+        image_paths = [x['path'] for ok, x in results if ok]
+        if image_paths:
+            item['bio_image'] = image_paths[0]
+
         return item
+
+
+#class NobelWinnersPipeline(object):
+#   def process_item(self, item, spider):
+#      return item
